641cf6e69145c5b436bf86f00ca0dcbec10ad37c
shahriarislam <shahriarislamrafi71@gmail.com>
2025-02-18 03:15:50 +0600

    setu done

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import app from './App';
+import config from './app/config';
+
+import mongoose from 'mongoose';
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
